5f9a2595fbf2d25aa054b45068091e1ef175846b
hotakus <ttowfive@gmail.com>
2023-09-06 11:42:19 +0800

    feat(NICO) add function

--- NIKO.gd
+++ NIKO.gd
@@ -1,11 +1,15 @@
 extends Node
 
 var a = 1
-
 var _1 = 12
+var hello = "hello world!!!"
+
+func add(x, y):
+	return (x + y)
+
 func _enter_tree():
 	_1 =_1+1
 	print(_1)
-	pass
-	print("hello world")
+	print(hello)
+	print(add(a, _1))
 	
